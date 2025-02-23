760b5ca3480f94ab245272adae967c44e4619329
Axwabo <axwabo@gmail.com>
2025-02-23 13:26:29 +0100

    extra positions in jail & jwm commands

--- ModeratorTools/Commands/Jail/JailCommand.cs
+++ ModeratorTools/Commands/Jail/JailCommand.cs
@@ -1,12 +1,40 @@
-﻿using ModeratorTools.Jail;
+﻿using Axwabo.Helpers;
+using ModeratorTools.Jail;
+using PlayerRoles.FirstPersonControl;
 
 namespace ModeratorTools.Commands.Jail;
 
 [CommandProperties(CommandHandlerType.RemoteAdmin, "jail", "Jails the specified players")]
 [ModeratorPermissions("jail.jail", PlayerPermissions.PlayersManagement)]
-public class JailCommand : SeparatedTargetingCommand
+[Usage("[index]")]
+public class JailCommand : SeparatedTargetingCommand, ITargetingPreExecutionFilter
 {
 
-    protected override CommandResult ExecuteOn(ReferenceHub target, ArraySegment<string> arguments, CommandSender sender) => target.TryJail(sender);
+    protected Vector3? TargetPosition;
+
+    public virtual CommandResult? OnBeforeExecuted(List<ReferenceHub> targets, ArraySegment<string> arguments, CommandSender sender)
+    {
+        TargetPosition = null;
+        if (arguments.Count == 0)
+            return CommandResult.Null;
+        if (!arguments.ParseInt(out var index))
+            return "!Invalid index.";
+        if (index == 0)
+            return CommandResult.Null;
+        var result = JailPositionValidator.TryGetCustomJailPosition(index - 1, out var position);
+        if (!result)
+            return result;
+        TargetPosition = position;
+        return CommandResult.Null;
+    }
+
+    protected override CommandResult ExecuteOn(ReferenceHub target, ArraySegment<string> arguments, CommandSender sender)
+    {
+        if (!target.TryJail(sender))
+            return false;
+        if (TargetPosition.HasValue)
+            target.TryOverridePosition(TargetPosition.Value);
+        return true;
+    }
 
 }
--- ModeratorTools/Commands/Jail/JailWithMe.cs
+++ ModeratorTools/Commands/Jail/JailWithMe.cs
@@ -1,17 +1,23 @@
 using ModeratorTools.Jail;
+using PlayerRoles.FirstPersonControl;
 
 namespace ModeratorTools.Commands.Jail;
 
 [CommandProperties(CommandHandlerType.RemoteAdmin, "jailWithMe", "Jails you and the specified players", "jwm")]
 [PlayerOnlyCommand]
-public sealed class JailWithMe : JailCommand, ITargetingPreExecutionFilter, ICustomResultCompiler
+public sealed class JailWithMe : JailCommand, ICustomResultCompiler
 {
 
     private bool _senderJailed;
 
-    public CommandResult? OnBeforeExecuted(List<ReferenceHub> targets, ArraySegment<string> arguments, CommandSender sender)
+    public override CommandResult? OnBeforeExecuted(List<ReferenceHub> targets, ArraySegment<string> arguments, CommandSender sender)
     {
+        var result = base.OnBeforeExecuted(targets, arguments, sender);
+        if (result.HasValue)
+            return result;
         _senderJailed = sender.Hub().TryJail();
+        if (_senderJailed && TargetPosition.HasValue)
+            sender.Hub().TryOverridePosition(TargetPosition.Value);
         return CommandResult.Null;
     }
 
