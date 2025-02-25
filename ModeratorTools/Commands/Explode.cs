7383ff7bc4567407605993d0cd0d852d5726cf43
Axwabo <axwabo@gmail.com>
2025-02-25 19:24:30 +0100

    explode command

---
+++ ModeratorTools/Commands/Explode.cs
@@ -0,0 +1,22 @@
+﻿using PlayerStatsSystem;
+using Utils;
+
+namespace ModeratorTools.Commands;
+
+[CommandProperties(CommandHandlerType.RemoteAdmin, "explode", "Instantly explodes the specified players")]
+[ModeratorPermissions("explode", PlayerPermissions.ForceclassToSpectator)]
+public sealed class Explode : SeparatedTargetingCommand
+{
+
+    private static readonly CustomReasonDamageHandler Handler = new("Exploded by an admin.");
+
+    protected override CommandResult ExecuteOn(ReferenceHub target, ArraySegment<string> arguments, CommandSender sender)
+    {
+        if (!target.TryGetPosition(out var position))
+            return false;
+        ExplosionUtils.ServerSpawnEffect(position, ItemType.GrenadeHE);
+        target.playerStats.KillPlayer(Handler);
+        return true;
+    }
+
+}
