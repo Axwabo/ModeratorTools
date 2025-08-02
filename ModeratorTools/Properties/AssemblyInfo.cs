b272abfd36b4f9c85fc50f6dbb4a35bf6bf5ce11
Axwabo <axwabo@gmail.com>
2025-08-02 10:51:34 +0200

    bump version + update LabAPI

--- ModeratorTools/Properties/AssemblyInfo.cs
+++ ModeratorTools/Properties/AssemblyInfo.cs
@@ -8,5 +8,5 @@
 [assembly: AssemblyCopyright("Copyright © 2025")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyVersion("1.0.1.0")]
+[assembly: AssemblyFileVersion("1.0.1.0")]
